f34ef5cdf09c796699c7fe587d9064b1f59dbcd5
pej.lelay <pej.lelay@gmail.com>
2019-09-24 10:54:52 +0900

    Break Timer now starts automatically

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import { Text, View, StyleSheet, TouchableOpacity, TextInput } from 'react-native'
 import {vibrate} from './utils'
 
-
+//This function is used for the userInput placeholders general settings
 function UserTextInput(props){
     return(
         <TextInput
@@ -22,13 +22,17 @@ export default class App extends Component {
       timer: null,
       minutes_Counter: '00',
       seconds_Counter: '00',
-      startDisable: false,
+      minBreak: '00',
+      secBreak: '00',
       titleText: 'Work Time: ',
       type:'work',
       storedWorkMinutes:'00',
       storedWorkSeconds:'00',
       storedBreakSeconds:'00',
       storedBreakMinutes:'00',
+      showWork: true,
+      showBreak: false,
+      secWarn:false
     }
   }
   componentWillUnmount() {
@@ -42,8 +46,16 @@ export default class App extends Component {
                   this.setState({seconds_Counter: '00'})
               }
               else{
-                  this.setState({seconds_Counter: input.length == 1 ? '0' + input : input,
-                      storedWorkSeconds: input.length == 1 ? '0' + input : input})
+                  if(Number(input) > 59){
+                      this.setState({seconds_Counter: '59',
+                          storedWorkSeconds: '59',
+                          secWarn:true})
+                  }
+                  else{
+                      this.setState({seconds_Counter: input.length == 1 ? '0' + input : input,
+                          storedWorkSeconds: input.length == 1 ? '0' + input : input,
+                          secWarn: false})
+                  }
               }
           }
           else{
@@ -51,7 +63,8 @@ export default class App extends Component {
                   this.setState({minutes_Counter: '00'})
               }
               else{
-                  this.setState({minutes_Counter: input.length == 1 ? '0' + input : input,
+                  this.setState({
+                      minutes_Counter: input.length == 1 ? '0' + input : input,
                       storedWorkMinutes: input.length == 1 ? '0' + input : input})
               }
           }
@@ -59,10 +72,19 @@ export default class App extends Component {
     else if(type==='break'){
           if(unit==='seconds'){
               if(input === ''){
-                  this.setState({storedBreakSeconds: '00'})
+                  this.setState({secBreak: '00'})
               }
               else{
-                  this.setState({storedBreakSeconds: input.length == 1 ? '0' + input : input})
+                  if(Number(input) > 59){
+                      this.setState({storedBreakSeconds: '59',
+                          secBreak: '59',
+                          secWarn: true})
+                  }
+                  else{
+                      this.setState({storedBreakSeconds: input.length == 1 ? '0' + input : input,
+                          secBreak: input.length == 1 ? '0' + input : input,
+                          secWarn: false})
+                  }
               }
           }
           else{
@@ -70,7 +92,8 @@ export default class App extends Component {
                   this.setState({storedBreakMinutes: '00'})
               }
               else{
-                  this.setState({storedBreakMinutes: input.length == 1 ? '0' + input : input})
+                  this.setState({storedBreakMinutes: input.length == 1 ? '0' + input : input,
+                      minBreak: input.length == 1 ? '0' + input : input})
               }
           }
       }
@@ -95,11 +118,10 @@ export default class App extends Component {
       if(this.state.seconds_Counter === '01' && this.state.minutes_Counter === '00'){
           vibrate()
       }
-        console.log(this.state.seconds_Counter)
-        console.log(this.state.minutes_Counter)
-        if(this.state.seconds_Counter == '00' && this.state.minutes_Counter == '00' && this.state.type == 'work') {
-            this.handleBreak()
-        }
+
+      if(this.state.minutes_Counter == 0 && this.state.seconds_Counter== 0 && this.state.type=='work'){
+          this.handleBreak()
+      }
 
       this.setState({
         minutes_Counter: count.length == 1 ? '0' + count : count,
@@ -107,30 +129,49 @@ export default class App extends Component {
       })
     }, 1000)
     this.setState({ timer })
-    this.setState({startDisable : true})
+
   }
 
   handleBreak = () => {
       clearInterval(this.state.timer)
-      this.setState({
-          minutes_Counter:'01',
-          seconds_Counter:this.state.storedBreakSeconds,
-          titleText:"Break Time:",
-          type:'break'
-      })
+      this.setState({titleText:'Break Time', showWork:false, showBreak:true})
+      let timer = setInterval(() => {
+          var num, count = this.state.minBreak
+          if(this.state.secBreak === '00' && count === '00'){
+              clearInterval(this.state.timer)
+              num = '00'
+          }
+          else{
+              num = (Number(this.state.secBreak) - 1).toString()
+          }
 
-      this.Start()
+          if (Number(this.state.secBreak) === 0 && count != 0) {
+              count = (Number(this.state.minBreak) - 1).toString()
+              num = '59'
+          }
+          if(this.state.secBreak === '01' && this.state.minBreak === '00'){
+              vibrate()
+          }
+
+          this.setState({
+              minBreak: count.length == 1 ? '0' + count : count,
+              secBreak: num.length == 1 ? '0' + num : num
+          })
+      }, 1000)
+      this.setState({ timer })
+
   }
 
   Stop = () => {
     clearInterval(this.state.timer)
-    this.setState({startDisable : false})
   }
 
   Clear = () => {
     this.timePicker('seconds','work',this.state.storedWorkSeconds)
     this.timePicker('minutes','work',this.state.storedWorkMinutes)
-      this.setState({titleText:'Work Time'})
+    this.timePicker('minutes','break',this.state.storedBreakMinutes)
+    this.timePicker('seconds','break',this.state.storedBreakSeconds)
+    this.setState({titleText:'Work Time',showWork:true, showBreak: false})
   }
 
 
@@ -139,7 +180,8 @@ export default class App extends Component {
         <View style={styles.container}>
           <Text style={{fontSize: 46,marginBottom:150}}>Podomoro Timer</Text>
           <Text style={{fontSize: 60, fontWeight: 'bold'}}>{this.state.titleText}</Text>
-          <Text style={{fontSize: 56, paddingBottom:10}}>{this.state.minutes_Counter}:{this.state.seconds_Counter}</Text>
+          {this.state.showWork && <Text style={{fontSize: 56, paddingBottom:10}}>{this.state.minutes_Counter}:{this.state.seconds_Counter}</Text>}
+          {this.state.showBreak && <Text style={{fontSize: 56, paddingBottom:10}}>{this.state.minBreak}:{this.state.secBreak}</Text>}
           <View style={styles.activeTimePicker}>
             <View>
                 <Text style={styles.selectText}>Work Time:</Text>
@@ -166,10 +208,11 @@ export default class App extends Component {
               </View>
           </View>
 
+          {this.state.secWarn && <Text style={{marginTop:-20, marginBottom:20, marginLeft:150}}>Seconds will be set as '59'</Text>}
+
           <View style={{flexDirection: 'row',justifyContent: 'space-between'}}>
             <TouchableOpacity
                 onPress={this.Start}
-                disabled={this.state.startDisable}
                 style={styles.buttons}>
               <Text style={styles.buttonText}>START</Text>
             </TouchableOpacity>
@@ -180,7 +223,6 @@ export default class App extends Component {
             </TouchableOpacity>
             <TouchableOpacity
                 onPress={this.Clear}
-                disabled={this.state.startDisable}
                 style={styles.buttons} >
               <Text style={styles.buttonText}>RESET</Text>
             </TouchableOpacity>
@@ -205,7 +247,6 @@ const styles = StyleSheet.create({
   buttons:{
     paddingHorizontal:25,
     paddingVertical:5,
-    // marginTop: 350,
     backgroundColor:'black',
     borderRadius: 7
   },
